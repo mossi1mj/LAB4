4467f4f6f62100b40026ad134cc165057b5d4c74
mossi1mj <mossjmyron@gmail.com>
2019-10-22 12:14:36 -0400

    second commit

---
+++ LAB4/LAB4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace LAB4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "y";
+            bool continueApp = true;
+            Console.WriteLine("Learn your Squares and Cubes!");
+            do
+            {
+                //prompt user for integer
+                Console.Write("Enter an integer: ");
+                int num = int.Parse(Console.ReadLine());
+
+                //input loop and shows calculations for squares and cubes
+                Console.WriteLine("Number\tSquared\tCubed\n" + "======\t=======\t=====");
+                for (int i = 1; i <= num; i++)
+                {
+                    int number = i;
+                    int squared = i * i;
+                    int cubed = i * i * i;
+                    Console.WriteLine($"{number}\t{squared}\t{cubed}");
+                }
+
+                //promt user to continue application
+                Console.WriteLine("Continue? (y/n)");
+                answer = Console.ReadLine().ToLower();
+
+                //loop if user enters wrong prompt to continue application
+                while (answer != "y" && answer != "n")
+                {
+                    Console.Write("Please enter either (y/n): ");
+                    answer = Console.ReadLine().ToLower();
+                }
+
+                //ends application if user chooses
+                if (answer == "n")
+                {
+                    Console.WriteLine("Good bye!");
+                    continueApp = false;
+                }
+
+            } while (continueApp);
+        }
+    }
+}
